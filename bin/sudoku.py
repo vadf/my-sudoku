6d1607b8c407963ff36e4c8034a3e97ecf8848d4
Vadim <vad.filippov@gmail.com>
2013-11-17 00:24:05 +0200

    Add constructor, get_row, check_row_duplicates methods.
    Change-Id: I6c3460fff305328293de97ad3ccebd46faa55f76

--- bin/sudoku.py
+++ bin/sudoku.py
@@ -1,9 +1,63 @@
+import re
+
+class FormatError(Exception):
+    pass
+
 class Sudoku:
     """Sudoku class"""
 
-    def __init__(self):
-         pass
+    def __init__(self, filename):
+        """(Sudoku, str) -> NoneType
+        
+        Reads Sudoku game field from a file.
+        Raises FormatError exception if game field is incorrect.
+        
+        >>> Sudoku('game_field.txt')
+        """
+        self.field = []
+        f = open(filename, 'r')
+        count = 0
+        pattern = re.compile('^[1-9*]{9}$')
+        for line in f:
+            if pattern.match(line):
+                count += 1
+                self.field.append([int(x) if x.isdigit() else 0 for x in line.strip()])
+            else:
+                f.close()
+                raise FormatError("Line '" + str(line) + "' doesn't match pattern '" + str(pattern) + "'")
+         
+        if count < 9:
+            f.close()
+            raise FormatError("Number of lines is " + str(count) + ", but should be 9")
+                 
+        f.close()
+
+    def get_row(self, num):
+        """(Sudoku, int) -> list of int
+        
+        Retrieve row num of game field 
+        
+        >>> sudoku = Sudoku("test_game_ok.txt")
+        >>> sudoku.get_row(0)
+        [6,0,0,4,0,0,0,0,2]
+        """
+        return self.field[num]
+
+    def check_row_duplicates(self, num):
+        """(Sudoku, int) -> bool
+        
+        Check row num in Sudoku field for duplicate values
+        
+        >>> sudoku = Sudoku("test_game_ok.txt")
+        >>> sudoku.check_row_duplicates(1)
+        False
+        """
+        row = self.get_row(num)
+        if sum(row) == sum(set(row)):
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
-    sudoku = Sudoku()
+    pass
 
--- test/test_sudoku.py
+++ test/test_sudoku.py
@@ -1,18 +1,45 @@
 import unittest
 import sudoku
-
+import os
 
 class TestSudoku(unittest.TestCase):
     """Sudoku testing class"""
 
+    test_game = "test_game_ok.txt"
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
     def setUp(self):
-        self.sudoku = sudoku.Sudoku()
+        self.sudoku = sudoku.Sudoku(self.dir_path + '/' + self.test_game)
 
     def tearDown(self):
         self.sudoku = None
 
-    def test_one(self):
-        self.assertTrue(True)
+    def test_init_wrong_format1(self):
+        """Test load game file with incorrect field size (expected 9*9)"""
+        self.assertRaises(sudoku.FormatError, sudoku.Sudoku, self.dir_path + '/' + 'test_game_nok1.txt')
+
+    def test_init_wrong_format2(self):
+        """Test load game file with incorrect field size (expected 9*9)"""
+        self.assertRaises(sudoku.FormatError, sudoku.Sudoku, self.dir_path + '/' + 'test_game_nok2.txt')
+
+    def test_init_wrong_format3(self):
+        """Test load game file with incorrect character (expected [0-9*])"""
+        self.assertRaises(sudoku.FormatError, sudoku.Sudoku, self.dir_path + '/' +'test_game_nok3.txt')
+
+    def test_get_row(self):
+        """Test that specific row can be retrieved"""
+        expected = [6,0,0,4,0,0,0,0,2]
+        actual = self.sudoku.get_row(0)
+        self.assertEqual(actual, expected)
+        expected = [4,0,0,0,0,1,0,0,8]
+        actual = self.sudoku.get_row(8)
+        self.assertEqual(actual, expected)
+
+    def test_check_row_ok(self):
+        """Test that row X doesn't contain duplicate values"""
+        self.assertTrue(self.sudoku.check_row_duplicates(1))
+        self.assertTrue(self.sudoku.check_row_duplicates(4))
+
 
 if __name__ == '__main__':
     unittest.main()
